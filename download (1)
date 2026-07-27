596aa49c595bb62abc79cc290aea721e66e6d38f
Kuna Ruthika <ruthikakuna2006@gmail.com>
2026-07-27 22:41:43 +0530

    initialize the springboot project

---
+++ download (1)
@@ -0,0 +1,13 @@
+package com.being_infinity.springboot_beginner_cohort_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBeginnerCohort3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ springboot-beginner-cohort-3/src/main/java/com/being_infinity/springboot_beginner_cohort_3/SpringbootBeginnerCohort3Application.java
@@ -0,0 +1,13 @@
+package com.being_infinity.springboot_beginner_cohort_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBeginnerCohort3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBeginnerCohort3Application.class, args);
+	}
+
+}
