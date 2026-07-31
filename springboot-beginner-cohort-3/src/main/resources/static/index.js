30199107595f7e8a45af2f43793389649773e8e8
Kuna Ruthika <ruthikakuna2006@gmail.com>
2026-07-31 21:07:27 +0530

    Created togglecard

--- springboot-beginner-cohort-3/src/main/resources/static/index.js
+++ springboot-beginner-cohort-3/src/main/resources/static/index.js
@@ -1,1 +1,25 @@
-alert("java script");
+//alert("hello from java script");
+var users=[
+    {
+        "image":"/images/john.png",
+        "name":"John Doe",
+        "gender":"MALE"
+    },
+    {
+        "image":"/images/jane.png",
+        "name":"Jane Doe",
+        "gender":"FEMALE"
+    }
+]
+var id=0;
+function toggleuser(){
+    id=(id+1) % users.length;
+    var userImage = document.getElementById("user-image");
+     var userName = document.getElementById("user-name");
+     var userGender = document.getElementById("user-gender");
+     
+     userImage.scr = users[id].image;
+     userName.innerHTML = users[id].name;
+     userGender.innerHTML = users[id].gender;
+     
+}
